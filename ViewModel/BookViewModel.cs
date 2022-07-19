6b5297ec5d429853fd16f7db0c896051ab32a016
Hancer Mercede <hancermercede@ochoa.com.do>
2022-07-19 12:58:06 -0400

    Add project files.

---
+++ ViewModel/BookViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLabraryProject.ViewModel
+{
+    public class BookViewModel
+    {
+        public int BookId { get; set; }
+        public string? About { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
